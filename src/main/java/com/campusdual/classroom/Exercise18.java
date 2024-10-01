db2814e88e39c8efa2bc0cfbaaefb63f53cb791a
Orlando Jose Garces Casal <ojgcasal@gmail.com>
2024-10-01 14:25:34 +0200

    Terminado

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,31 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
 public class Exercise18 {
 
 	public static void main(String[] args) {
 
+		int size = Utils.integer("Ingrese la cantidad de números naturales que desea en el array: ");
+
+		int[] array = createAndInitializeArray(size);
+
+		showInlineArray(array);
+	}
+
+	public static int[] createAndInitializeArray(int n) {
+		int[] array = new int[n];
+		for (int i = 0; i < n; i++) {
+			array[i] = i + 1;
+		}
+		return array;
+	}
+
+	public static void showInlineArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+			if (i < array.length - 1) {
+				System.out.print(" ");
+			}
+		}
 	}
 }
